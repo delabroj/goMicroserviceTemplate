0743920e6f9a11194479eedb1534386436c9c453
delabroj <delabroj@users.noreply.github.com>
2017-06-16 11:12:03 -0600

    wrap DefaultServeMux with logRequest middleware

--- goMicroserviceTemplate.go
+++ goMicroserviceTemplate.go
@@ -9,7 +9,5 @@ import (
 func main() {
 	http.HandleFunc("/status", status)
 	http.Handle("/time", message(time.Now().Format(time.RFC1123)))
-	http.Handle("/status-log", logRequest(http.HandlerFunc(status)))
-	http.Handle("/time-log", logRequest(message(time.Now().Format(time.RFC1123))))
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	log.Fatal(http.ListenAndServe(":8000", logRequest(http.DefaultServeMux)))
 }
